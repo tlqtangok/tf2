794eced375bcc1525a716776c1134f7d4e4990ab
Jidor Tang <tlqtangok@126.com>
2019-05-24 11:26:11 +0800

    - edit code_.py at 20190524_1126

--- py_mess/code_.py
+++ py_mess/code_.py
@@ -1916,3 +1916,65 @@ print(id_predict)
 
 ===
 
+### a really simple example to use libcudnn and plot loss via id_pd.plot.scatter
+
+import tensorflow as tf 
+
+from tensorflow.keras.models import Sequential, Model
+from tensorflow.keras.layers import Embedding
+from tensorflow.keras.layers import Input, Activation, Dense, Permute, Dropout, Conv2D, Flatten, Reshape
+from tensorflow.keras.layers import add, dot, concatenate
+from tensorflow.keras.layers import LSTM
+from tensorflow.keras.utils import get_file
+from tensorflow.keras.preprocessing.sequence import pad_sequences
+from tensorflow.keras.utils import plot_model
+
+from functools import reduce
+import tarfile
+import numpy as np
+import pandas as pd
+import re
+import os
+from sklearn.model_selection import train_test_split
+
+from IPython.display import display
+from PIL import Image
+import matplotlib.pyplot as plt
+
+
+
+x_train_all =  np.random.random([10, 28,28,1])
+y_label_all =  np.random.random([10, 26,26,1])
+
+x_train, y_test, x_label, y_label = train_test_split(x_train_all, y_label_all, test_size=0.2)
+
+model = Sequential(
+[
+    Conv2D(4, (3,3), activation="relu", input_shape=x_train.shape[1:]), 
+    Dense(1)
+]
+)
+
+model.summary()
+
+model.compile(loss='mse', optimizer='adam')
+
+history = model.fit(
+    x_train, x_label, 
+    validation_data=(y_test, y_label),
+    epochs=32,
+    batch_size=10,
+    verbose=0
+)
+
+id_pd = pd.DataFrame(history.history)
+
+
+id_pd["idx"] = np.arange(len(id_pd))
+
+# display(id_pd)
+
+id_pd.plot.scatter(x=["idx"] * 2,y=["loss", "val_loss"], c=["blue", "red"])
+plt.show()
+
+=== 
